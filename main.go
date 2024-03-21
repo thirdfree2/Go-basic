9f01bdd4ceb3f4e64a10eaebefd2288a3b7eedea
thirdfree2 <pativeth.k@gmail.com>
2024-03-21 21:37:02 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Pativeth"
+	var age int = 25
+	var score float32 = 98.5
+	var isPass bool = true
+	fmt.Println("My name is ",name)
+	fmt.Println("Age = ",age)
+	fmt.Println("Score ",score)
+	fmt.Println("Pass Exam ",isPass)
+}
+
